553c47758a421798905e602dd1266985e1ebbe8c
atomtables <76572470+atomtables@users.noreply.github.com>
2024-05-09 11:17:44 -0400

    Initial Commit

---
+++ DiscordForMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiscordForMac
+//
+//  Created by Adithiya Venkatakrishnan on 9/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiscordForMac/DiscordForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordForMacApp.swift
+//  DiscordForMac
+//
+//  Created by Adithiya Venkatakrishnan on 9/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscordForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
